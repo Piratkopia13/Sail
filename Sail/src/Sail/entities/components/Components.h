d5cde812a27ed846084714f9b169b6effa402a61
Viktor <viktor.enfeldt@gmail.com>
2019-09-16 10:56:48 +0200

    deleted the now unused transformMatrix and -Data components

--- Sail/src/Sail/entities/components/Components.h
+++ Sail/src/Sail/entities/components/Components.h
@@ -4,8 +4,6 @@
 #include "TransformComponent.h"
 #include "ModelComponent.h"
 #include "TextComponent.h"
-#include "TransformMatrixComponent.h"
-#include "TransformDataComponent.h"
 #include "PhysicsComponent.h"
 //#include "MovementComponent.h"
 #include "PlayerComponent.h"
