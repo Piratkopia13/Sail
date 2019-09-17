5a982b341da3e9d99a54d8cf19632b4489c3cff5
Fredrik Junede <frju15@student.bth.se>
2019-09-17 13:41:34 +0200

    Added a simple steering behaviour to the AiController

--- SPLASH/src/game/controllers/AiController.cpp
+++ SPLASH/src/game/controllers/AiController.cpp
@@ -3,7 +3,7 @@
 #include "Sail.h"
 
 AiController::AiController()
-	: m_movementSpeed(2.f)
+	: m_movementSpeed(5.f)
 	, m_timeBetweenPathUpdate(1.0f /*seconds*/)
 	, m_timeTaken(0.f)
 	, m_currNodeIndex(-1)
@@ -14,7 +14,10 @@ AiController::AiController()
 	, m_entityTarget(nullptr)
 	, m_lastTargetPos(glm::vec3(FLT_MAX, FLT_MAX, FLT_MAX))
 	, m_target(glm::vec3(0.f, 0.f, 0.f))
+	, m_targetReachedThreshold(0.3f)
 	, m_reachedTarget(true)
+	, m_maxSteeringForce(0.3f)
+	, m_mass(5.f)
 {
 }
 
@@ -43,16 +46,28 @@ void AiController::update(float dt) {
 		m_reachedTarget = false;
 	}
 	
-	if (!m_reachedTarget) {
-		auto dir = m_currPath[m_currNodeIndex].position - m_transComp->getTranslation();
-		if ( dir == glm::vec3(0.f) ) {
-			dir = glm::vec3(1.0f, 0.f, 0.f);
-		}
-		dir = glm::normalize(dir);
-		m_physComp->velocity = dir * m_movementSpeed;
-		if (glm::distance(m_transComp->getTranslation(), m_currPath[m_currNodeIndex].position) < 1.f) {
+	if ( !m_reachedTarget ) {
+		// Check if the distance between target and ai is low enough
+		if ( glm::distance(m_transComp->getTranslation(), m_currPath[m_currNodeIndex].position) < m_targetReachedThreshold ) {
 			m_lastVisitedNode = m_currPath[m_currNodeIndex];
 			m_reachedTarget = true;
+		}
+		else {
+			glm::vec3 desiredDir = m_currPath[m_currNodeIndex].position - m_transComp->getTranslation();
+			if ( desiredDir == glm::vec3(0.f) ) {
+				desiredDir = glm::vec3(1.0f, 0.f, 0.f);
+			}
+			desiredDir = glm::normalize(desiredDir);
+			glm::vec3 desiredVel = desiredDir * m_movementSpeed;
+			glm::vec3 steering = ( desiredVel - m_physComp->velocity );
+			float steeringMag = ( glm::length(steering) * m_mass );
+			steering *= m_maxSteeringForce / (steeringMag != 0.f ? steeringMag : 1.f);
+			m_physComp->velocity += steering;
+			// Float used to clamp the magnitude of the velocity between 0 and m_movementSpeed
+			float velMagClamper = glm::length(m_physComp->velocity);
+			velMagClamper = ( velMagClamper > m_movementSpeed ) ? m_movementSpeed / velMagClamper : 1.0f;
+			m_physComp->velocity = m_physComp->velocity * velMagClamper;
+			//Logger::Log("Velocity: " + Utils::toStr(m_physComp->velocity));
 		}
 	}
 	else {
@@ -105,11 +120,11 @@ void AiController::updatePath() {
 
 		m_currPath = tempPath;
 
-		std::string toPrint = "[";
+		/*std::string toPrint = "[";
 		for ( auto p : m_currPath ) {
 			toPrint += (std::to_string(p.index) + ", ");
 		}
 		toPrint += "]";
-		Logger::Log(toPrint);
+		Logger::Log(toPrint);*/
 	}
 }
