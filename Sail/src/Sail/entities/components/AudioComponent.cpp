e5e4260f519e691460ef3044084391493101fa2e
OLIVER GLANDBERGER <olgl14@student.bth.se>
2019-09-24 15:34:22 +0200

    Created the class and general 'starting' structure, as well as made sure that code is still runnable. Pushing before continuing.

---
+++ Sail/src/Sail/entities/components/AudioComponent.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+
+#include "AudioComponent.h"
+
+AudioComponent::AudioComponent() {
+
+}
+
+AudioComponent::~AudioComponent() {
+
+}
+
---
+++ Sail/src/Sail/entities/components/AudioComponent.h
@@ -0,0 +1,16 @@
+#ifndef AUDIO_COMPONENT_H
+#define AUDIO_COMPONENT_H
+
+#include "Sail/entities/components/Component.h"
+
+class AudioComponent : public Component<AudioComponent>
+{
+public:
+	AudioComponent();
+	virtual ~AudioComponent();
+
+private:
+
+};
+
+#endif
