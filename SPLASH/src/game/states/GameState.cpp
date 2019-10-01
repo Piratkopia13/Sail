13669805e72196856299e458e739824152aac0ad
Daniel Fredriksson <daniel.ls.fredriksson@gmail.com>
2019-10-01 15:42:44 +0200

    Deleted botFactory class as it does nothing.

--- SPLASH/src/game/states/GameState.cpp
+++ SPLASH/src/game/states/GameState.cpp
@@ -407,7 +407,7 @@ GameState::GameState(StateStack& stack)
 		e->addComponent<CollidableComponent>();
 		e->addComponent<PhysicsComponent>();
 		e->addComponent<AiComponent>();
-		e->addComponent<GunComponent>(cubeModel, boundingBoxModel);
+		e->addComponent<GunComponent>(cubeModel, boundingBoxModel);.
 		e->addChildEntity(createCandleEntity("AiCandle", lightModel, boundingBoxModel, glm::vec3(0.f, 2.f, 0.f)));
 		
 
