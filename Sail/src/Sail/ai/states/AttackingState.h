29c7d0c394096a2f3b66ae0797a09f5514c13000
Fredrik Junede <frju15@student.bth.se>
2019-10-01 14:57:20 +0200

    Added the attacking state

---
+++ Sail/src/Sail/ai/states/AttackingState.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "State.h"
+#include "Sail/utils/Utils.h"
+
+class AttackingState : public FSM::State<AttackingState> {
+public:
+	AttackingState() {
+
+	}
+	~AttackingState() {
+
+	}
+
+	void update(float dt) override {
+		Logger::Log("I am Attacking State, hello!");
+	}
+
+	virtual void reset() {}
+
+	virtual void init() {}
+
+
+
+private:
+
+};
