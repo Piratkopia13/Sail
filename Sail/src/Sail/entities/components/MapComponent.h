fec626f8c7aa444ba5b9a48c8e0b969509c70969
Fredrik Junede <frju15@student.bth.se>
2019-10-11 10:47:00 +0200

    Fixed a define

--- Sail/src/Sail/entities/components/MapComponent.h
+++ Sail/src/Sail/entities/components/MapComponent.h
@@ -41,7 +41,7 @@ public:
 		}
 
 	}
-//#ifdef _PERFORMANCE_TEST
+#ifdef _PERFORMANCE_TEST
 	const static int xsize = 50, ysize = 50; //size of level
 	int tileArr[xsize][ysize][3]; //0 is tileID, 1 is typeID, 2 is door
 	float hallwayThreshold = 0.3f; // percentage of level that can be corridors
@@ -51,21 +51,21 @@ public:
 	int roomSplitStop = 25;//percentage to stop a room from being split into smaller ones
 	int doorModifier = 15;//percentage to spawn a door
 	int seed = 2;//seed for generation
-//#else
-//#ifdef _DEBUG
-//	const static int xsize = 3, ysize = 3; //size of level
-//#else
-	//const static int xsize = 15, ysize = 15; //size of level
-//#endif
-//	int tileArr[xsize][ysize][3]; //0 is tileID, 1 is typeID, 2 is door
-//	float hallwayThreshold = 0.3f; // percentage of level that can be corridors
-//	int minSplitSize = 5; //minimum size for splitting chunks
-//	int minRoomSize = 1; //minimum side of a room
-//	int roomMaxSize = 36;//maximum area of a room
-//	int roomSplitStop = 25;//percentage to stop a room from being split into smaller ones
-//	int doorModifier = 15;//percentage to spawn a door
-//	int seed = 2;//seed for generation
-//#endif
+#else
+#ifdef _DEBUG
+	const static int xsize = 3, ysize = 3; //size of level
+#else
+	const static int xsize = 15, ysize = 15; //size of level
+#endif
+	int tileArr[xsize][ysize][3]; //0 is tileID, 1 is typeID, 2 is door
+	float hallwayThreshold = 0.3f; // percentage of level that can be corridors
+	int minSplitSize = 5; //minimum size for splitting chunks
+	int minRoomSize = 1; //minimum side of a room
+	int roomMaxSize = 36;//maximum area of a room
+	int roomSplitStop = 25;//percentage to stop a room from being split into smaller ones
+	int doorModifier = 15;//percentage to spawn a door
+	int seed = 2;//seed for generation
+#endif
 	int totalArea = xsize * ysize;
 	std::queue<rect> chunks, blocks, hallways, rooms, matched;
 private:
