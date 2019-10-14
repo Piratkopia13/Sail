1494b1207d9bc3123721d60a67901fabe60cf892
Fredrik Junede <frju15@student.bth.se>
2019-10-14 10:30:15 +0200

    Fixed return in AiSystem

--- Sail/src/Sail/entities/systems/Gameplay/ai/AiSystem.cpp
+++ Sail/src/Sail/entities/systems/Gameplay/ai/AiSystem.cpp
@@ -143,7 +143,7 @@ void AiSystem::aiUpdateFunc(Entity* e, const float dt) {
 	updatePhysics(e, dt);
 }
 
-glm::vec3& AiSystem::getDesiredDir(AiComponent* aiComp, TransformComponent* transComp) {
+glm::vec3 AiSystem::getDesiredDir(AiComponent* aiComp, TransformComponent* transComp) {
 	glm::vec3 desiredDir = aiComp->currPath[aiComp->currNodeIndex].position - transComp->getTranslation();
 	if ( desiredDir == glm::vec3(0.f) ) {
 		desiredDir = glm::vec3(1.0f, 0.f, 0.f);
