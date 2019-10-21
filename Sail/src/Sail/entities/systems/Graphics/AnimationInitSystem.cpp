a36f9e1bebd2ea6c76ebfe0208a56077913e2a25
Henrik Johansson <H3nx@hotmail.se>
2019-10-21 23:29:29 +0200

    Fixed fatfinger error..

--- Sail/src/Sail/entities/systems/Graphics/AnimationInitSystem.cpp
+++ Sail/src/Sail/entities/systems/Graphics/AnimationInitSystem.cpp
@@ -31,7 +31,7 @@ void AnimationInitSystem::initAnimations() {
 		animationEntity2->addComponent<TransformComponent>();
 		animationEntity2->getComponent<TransformComponent>()->translate(-2.0f+i*1.2f, 0, 0);
 		Model* model = &app->getResourceManager().getModelCopy(name);
-		animationEntity2->addComponent<ModelComponent>(model
+		animationEntity2->addComponent<ModelComponent>(model);
 		model->getMesh(0)->getMaterial()->setMetalnessRoughnessAOTexture("pbr/Character/CharacterMRAO.tga");
 		model->getMesh(0)->getMaterial()->setAlbedoTexture("pbr/Character/CharacterTex.tga");
 		model->getMesh(0)->getMaterial()->setNormalTexture("pbr/Character/CharacterNM.tga");
