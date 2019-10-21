8c704dee21f4d352381f24bed5980ef99f9d215f
Henrik Johansson <H3nx@hotmail.se>
2019-10-21 23:28:13 +0200

    Removed older models Added model with animationstack Updated preview to use new model

--- Sail/src/Sail/entities/systems/Graphics/AnimationInitSystem.cpp
+++ Sail/src/Sail/entities/systems/Graphics/AnimationInitSystem.cpp
@@ -22,13 +22,23 @@ void AnimationInitSystem::loadAnimations() {
 void AnimationInitSystem::initAnimations() {
 	Application* app = Application::getInstance();
 	auto* shader = &app->getResourceManager().getShaderSet<GBufferOutShader>();
+	std::string name = "Docfbx.fbx";
 
-	auto animationEntity2 = ECS::Instance()->createEntity("DocGunRun4");
-	animationEntity2->addComponent<TransformComponent>();
-	animationEntity2->getComponent<TransformComponent>()->translate(0, 0, 0);
-	animationEntity2->addComponent<ModelComponent>(&app->getResourceManager().getModelCopy("DocGunRun4.fbx"));
-	animationEntity2->getComponent<ModelComponent>()->getModel()->setIsAnimated(true);
-	animationEntity2->addComponent<AnimationComponent>(&app->getResourceManager().getAnimationStack("DocGunRun4.fbx"));
-	animationEntity2->getComponent<AnimationComponent>()->currentAnimation = animationEntity2->getComponent<AnimationComponent>()->getAnimationStack()->getAnimation(0);
+	AnimationStack* stack = &app->getResourceManager().getAnimationStack(name);
+	unsigned int animationCount = stack->getAnimationCount();
+	for (unsigned int i = 0; i < animationCount; i++) {
+		auto animationEntity2 = ECS::Instance()->createEntity("Doc" + std::to_string(i));
+		animationEntity2->addComponent<TransformComponent>();
+		animationEntity2->getComponent<TransformComponent>()->translate(-2.0f+i*1.2f, 0, 0);
+		Model* model = &app->getResourceManager().getModelCopy(name);
+		animationEntity2->addComponent<ModelComponent>(model
+		model->getMesh(0)->getMaterial()->setMetalnessRoughnessAOTexture("pbr/Character/CharacterMRAO.tga");
+		model->getMesh(0)->getMaterial()->setAlbedoTexture("pbr/Character/CharacterTex.tga");
+		model->getMesh(0)->getMaterial()->setNormalTexture("pbr/Character/CharacterNM.tga");
+		animationEntity2->getComponent<ModelComponent>()->getModel()->setIsAnimated(true);
+		animationEntity2->addComponent<AnimationComponent>(stack);
+		animationEntity2->getComponent<AnimationComponent>()->currentAnimation = stack->getAnimation(i);
+
+	}
 
 }
