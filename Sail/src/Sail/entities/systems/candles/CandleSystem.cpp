467834c9123f2153f89ddff57fbbc3a38b708655
Fredrik Junede <frju15@student.bth.se>
2019-10-23 14:09:29 +0200

    Removed some unused code and updated look of kill feed

--- Sail/src/Sail/entities/systems/candles/CandleSystem.cpp
+++ Sail/src/Sail/entities/systems/candles/CandleSystem.cpp
@@ -73,13 +73,6 @@ void CandleSystem::update(float dt) {
 							}
 						);
 
-						// Print who killed who
-						unsigned char idOfDeadPlayer = static_cast<unsigned char>(e->getParent()->getComponent<NetworkReceiverComponent>()->m_id >> 18);
-						unsigned char idOfShootingPlayer = candle->getWasHitByNetID();
-						std::string deadPlayer = NWrapperSingleton::getInstance().getPlayer(idOfDeadPlayer)->name;
-						std::string ShooterPlayer = NWrapperSingleton::getInstance().getPlayer(idOfShootingPlayer)->name;
-						Logger::Log(ShooterPlayer + " sprayed down " + deadPlayer);
-
 						//This should remove the candle entity from game
 						e->getParent()->removeDeleteAllChildren();
 
--- Sail/src/Sail/utils/SailImGui/KillFeedWindow.cpp
+++ Sail/src/Sail/utils/SailImGui/KillFeedWindow.cpp
@@ -13,18 +13,18 @@ void KillFeedWindow::setDeaths(const std::vector<std::string>& kills) {
 
 void KillFeedWindow::renderWindow() {
 	if (m_kills.size() > 0) {
-		ImVec2 minSize = ImVec2(180.f, 30.f);
+		ImVec2 minSize = ImVec2(0.f, 0.f);
 		ImVec2 maxSize = ImVec2(240.f, 200.f);
 		ImGui::SetNextWindowSizeConstraints(minSize, maxSize);
 
-		ImVec2 textMinSize = ImVec2(180.f, 10.f);
-		ImVec2 textMaxSize = ImVec2(240.f, 10.f);
-		ImGui::Begin("Kill Feed", nullptr, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoScrollbar);
+		ImVec2 textMinSize = ImVec2(0, 0);
+		ImVec2 textMaxSize = ImVec2(240.f, 20.f);
+		ImGui::Begin("Kill Feed", nullptr, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoScrollbar);
 
 		ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.f, 0.f, 0.f, 0.05f));
 		for (int i = m_kills.size() - 1; i > -1; i--) {
 			ImGui::SetNextWindowSizeConstraints(textMinSize, textMaxSize);
-			ImGui::BeginChild("");
+			ImGui::BeginChild("", ImVec2(0.f, 0.f), false, ImGuiWindowFlags_AlwaysAutoResize);
 			ImGui::Text(m_kills[i].c_str());
 			ImGui::EndChild();
 		}
