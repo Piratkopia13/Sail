801c8abcbf090c4a02e9a0d925ac0987ad4f6efe
Henrik Johansson <H3NX@hotmail.se>
2019-10-25 15:07:00 +0200

    Moved pointlight to top of candle

--- Sail/src/Sail/entities/systems/candles/CandleSystem.cpp
+++ Sail/src/Sail/entities/systems/candles/CandleSystem.cpp
@@ -136,7 +136,7 @@ void CandleSystem::update(float dt) {
 			e->getComponent<LightComponent>()->getPointLight().setColor(glm::vec3(tempHealthRatio, tempHealthRatio * 0.7f, tempHealthRatio * 0.4f));
 
 			candle->setWasCarriedLastUpdate(candle->isCarried());
-			glm::vec3 flamePos = glm::vec3(e->getComponent<TransformComponent>()->getMatrix()[3]) + glm::vec3(0, 0.5f, 0);
+			glm::vec3 flamePos = e->getComponent<TransformComponent>()->getMatrix() * glm::vec4(0, 0.37f, 0, 1);
 			e->getComponent<LightComponent>()->getPointLight().setPosition(flamePos);
 		}
 	}
