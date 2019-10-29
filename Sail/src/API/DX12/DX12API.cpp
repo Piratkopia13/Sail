76701f72bb4310571ffecdca3d3e2af39045ea83
Alexander Wester <piratkopia13@gmail.com>
2019-10-29 10:07:19 +0100

    Added frameDependant parameter for descriptor heaps that splits its use into two

--- Sail/src/API/DX12/DX12API.cpp
+++ Sail/src/API/DX12/DX12API.cpp
@@ -351,9 +351,9 @@ void DX12API::createGlobalRootSignature() {
 void DX12API::createShaderResources() {
 	// TODO: maybe dont hardcode numdescriptors?
 	// Create one big gpu descriptor heap for all cbvs, srvs and uavs used on the graphics queue
-	m_cbvSrvUavDescriptorHeapGraphics = std::make_unique<DescriptorHeap>(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, 50000, true);
+	m_cbvSrvUavDescriptorHeapGraphics = std::make_unique<DescriptorHeap>(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, 50000, true, true);
 	// Create one big gpu descriptor heap for all cbvs, srvs and uavs used on the compute queue
-	m_cbvSrvUavDescriptorHeapCompute = std::make_unique<DescriptorHeap>(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, 50000, true);
+	m_cbvSrvUavDescriptorHeapCompute = std::make_unique<DescriptorHeap>(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, 50000, true, true);
 }
 
 void DX12API::createDepthStencilResources(Win32Window* window) {
