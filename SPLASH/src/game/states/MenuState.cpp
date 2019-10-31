8692bdddc08233513fd4e09e8fee08f7bff179b2
Fredrik Junede <frju15@student.bth.se>
2019-10-31 10:08:41 +0100

    Fixed host id Fixed lobby join issue

--- SPLASH/src/game/states/MenuState.cpp
+++ SPLASH/src/game/states/MenuState.cpp
@@ -117,6 +117,7 @@ bool MenuState::renderImgui(float dt) {
 			if (ImGui::Button("Host Game")) {
 				if (m_network->host()) {
 					// Update server description after host added himself to the player list.
+					NWrapperSingleton::getInstance().getMyPlayer().id = HOST_ID;
 					NWrapperSingleton::getInstance().playerJoined(NWrapperSingleton::getInstance().getMyPlayer());
 					NWrapperHost* wrapper = static_cast<NWrapperHost*>(NWrapperSingleton::getInstance().getNetworkWrapper());
 					if (lobbyName == "") {
@@ -170,7 +171,7 @@ bool MenuState::renderImgui(float dt) {
 			ImGui::Separator();
 
 			int index = 0;
-			if (selected > m_foundLobbies.size()) {
+			if (selected >= m_foundLobbies.size()) {
 				selected = -1;
 			}
 			for (auto& lobby : m_foundLobbies) {
