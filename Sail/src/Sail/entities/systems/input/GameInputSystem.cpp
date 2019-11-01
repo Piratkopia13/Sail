c2d28003c28f7b7f674653fde6cdfbecbf8bfbf9
Viktor <viktor.enfeldt@gmail.com>
2019-11-01 16:20:44 +0100

    Made all players auto-fire for debug reasons

--- Sail/src/Sail/entities/systems/input/GameInputSystem.cpp
+++ Sail/src/Sail/entities/systems/input/GameInputSystem.cpp
@@ -331,9 +331,9 @@ void GameInputSystem::processMouseInput(const float& dt) {
 			}
 		}
 
-		if (!e->hasComponent<SpectatorComponent>() && Input::IsMouseButtonPressed(KeyBinds::SHOOT)) {
-			GunComponent* gc = e->getComponent<GunComponent>();
-			TransformComponent* ptc = e->getComponent<TransformComponent>();
+		//if (!e->hasComponent<SpectatorComponent>() && Input::IsMouseButtonPressed(KeyBinds::SHOOT)) {
+		//	GunComponent* gc = e->getComponent<GunComponent>();
+		//	TransformComponent* ptc = e->getComponent<TransformComponent>();
 			if (gc) {
 				for (auto childE : e->getChildEntities()) {
 					if (childE->getName().find("WaterGun") != std::string::npos) {
@@ -344,15 +344,15 @@ void GameInputSystem::processMouseInput(const float& dt) {
 					}
 				}
 			}
-		}
-		else {
+		//}
+		//else {
 
-			GunComponent* gc = e->getComponent<GunComponent>();
-			if (gc) {
-				gc->firing = false;
-			}
+		//	GunComponent* gc = e->getComponent<GunComponent>();
+		//	if (gc) {
+		//		gc->firing = false;
+		//	}
 
-		}
+		//}
 
 	}
 }
