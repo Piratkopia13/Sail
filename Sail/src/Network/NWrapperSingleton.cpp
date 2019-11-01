7b5bf87d500bde968bb502675ccb196bd636796c
Daniel Fredriksson <daniel.ls.fredriksson@gmail.com>
2019-11-01 10:02:58 +0100

    Added errorlogger for invalid events

--- Sail/src/Network/NWrapperSingleton.cpp
+++ Sail/src/Network/NWrapperSingleton.cpp
@@ -162,6 +162,10 @@ void NWrapperSingleton::setNSS(NetworkSenderSystem* NSS_) {
 }
 
 void NWrapperSingleton::queueGameStateNetworkSenderEvent(Netcode::MessageType type, Netcode::MessageData* data, bool alsoSendToSelf) {
+	if (type < Netcode::MessageType::CREATE_NETWORKED_ENTITY || type < Netcode::MessageType::COUNT) {
+		Logger::Error("TRIED TO QUEUE AN INVALID EVENT: (" + std::to_string((int)type->type) + ")");
+	}
+
 	// Cleaning is handled by the NSS later on.
 	NetworkSenderEvent* e = SAIL_NEW NetworkSenderEvent;
 	e->type = type;
