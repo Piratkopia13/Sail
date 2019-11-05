0f3baeac38d8a80a332050fbc11deaf2672b705a
Henrik Johansson <H3nx@hotmail.se>
2019-11-05 23:50:02 +0100

    added saftblandare to splashscreen

--- SPLASH/src/game/states/SplashScreenState.cpp
+++ SPLASH/src/game/states/SplashScreenState.cpp
@@ -166,5 +166,10 @@ bool SplashScreenState::loadTextures(Application* app) {
 	rm->loadTexture("pbr/Clutter/SO_MRAO.tga");
 	rm->loadTexture("pbr/Clutter/SO_NM.tga");
 	rm->loadTexture("pbr/Clutter/SO_Albedo.tga");
+
+	rm->loadTexture("pbr/Clutter/Saftblandare_MRAO.tga");
+	rm->loadTexture("pbr/Clutter/Saftblandare_NM.tga");
+	rm->loadTexture("pbr/Clutter/Saftblandare_Albedo.tga");
+
 	return false;
 }
