a97d8b2d2d0bb9615d37156b10f1b1f21d7954c5
Gustav Björk <gbjorken96@hotmail.com>
2019-11-08 14:20:42 +0100

    Cleaned up particle compute shader.

--- SPLASH/res/shaders/compute/ParticleComputeShader.hlsl
+++ SPLASH/res/shaders/compute/ParticleComputeShader.hlsl
@@ -38,56 +38,88 @@ struct ParticlePhysics {
 
 RWStructuredBuffer<ParticlePhysics> CSPhysicsBuffer: register(u11) : SAIL_IGNORE;
 
-void createTriangle(float3 v0, float3 v1, float3 v2, int v0Index) {
+void createParticle(float3 v0, float3 v1, float3 v2, float3 v3, int particleIndex) {
+	int v0Index = particleIndex * 6;
+	
+	//Triangle 1
 	CSOutputBuffer[v0Index].position = v0;
-	CSOutputBuffer[v0Index].texCoords = (0.f, 0.f);
+	CSOutputBuffer[v0Index].texCoords = float2(0.f, 0.f);
 	CSOutputBuffer[v0Index].normal = 0.f;
 	CSOutputBuffer[v0Index].tangent = 0.f;
 	CSOutputBuffer[v0Index].bitangent = 0.f;
 	
 	CSOutputBuffer[v0Index + 1].position = v1;
-	CSOutputBuffer[v0Index + 1].texCoords = 0.f;
+	CSOutputBuffer[v0Index + 1].texCoords = float2(0.f, 1.f);
 	CSOutputBuffer[v0Index + 1].normal = 0.f;
 	CSOutputBuffer[v0Index + 1].tangent = 0.f;
 	CSOutputBuffer[v0Index + 1].bitangent = 0.f;
 	
 	CSOutputBuffer[v0Index + 2].position = v2;
-	CSOutputBuffer[v0Index + 2].texCoords = 0.f;
+	CSOutputBuffer[v0Index + 2].texCoords = float2(1.f, 0.f);
 	CSOutputBuffer[v0Index + 2].normal = 0.f;
 	CSOutputBuffer[v0Index + 2].tangent = 0.f;
 	CSOutputBuffer[v0Index + 2].bitangent = 0.f;
+	
+	//Triangle 2
+	CSOutputBuffer[v0Index + 3].position = v2;
+	CSOutputBuffer[v0Index + 3].texCoords = float2(1.f, 0.f);
+	CSOutputBuffer[v0Index + 3].normal = 0.f;
+	CSOutputBuffer[v0Index + 3].tangent = 0.f;
+	CSOutputBuffer[v0Index + 3].bitangent = 0.f;
+	
+	CSOutputBuffer[v0Index + 4].position = v1;
+	CSOutputBuffer[v0Index + 4].texCoords = float2(0.f, 1.f);
+	CSOutputBuffer[v0Index + 4].normal = 0.f;
+	CSOutputBuffer[v0Index + 4].tangent = 0.f;
+	CSOutputBuffer[v0Index + 4].bitangent = 0.f;
+	
+	CSOutputBuffer[v0Index + 5].position = v3;
+	CSOutputBuffer[v0Index + 5].texCoords = float2(1.f, 1.f);
+	CSOutputBuffer[v0Index + 5].normal = 0.f;
+	CSOutputBuffer[v0Index + 5].tangent = 0.f;
+	CSOutputBuffer[v0Index + 5].bitangent = 0.f;
+}
+
+void updatePhysics(int particleIndex, float dt) {
+	float3 oldVelocity = CSPhysicsBuffer[particleIndex].velocity;
+	CSPhysicsBuffer[particleIndex].velocity += CSPhysicsBuffer[particleIndex].acceleration * dt;
+	for (uint j = 0 ; j < 6; j++) {
+		CSOutputBuffer[particleIndex * 6 + j].position += (oldVelocity + CSPhysicsBuffer[particleIndex].velocity) * 0.5 * dt;
+	}
 }
 
 [numthreads(1, 1, 1)]
 void CSMain(int3 groupThreadID : SV_GroupThreadID, int3 dispatchThreadID : SV_DispatchThreadID) {
-	int counter = 0;
+	int counter = 0; // New particle counter
     for (uint i = 0; i < inputBuffer.numEmitters; i++) {
-		for (uint j = 0; j < min(inputBuffer.emitters[i].nrOfParticlesToSpawn, inputBuffer.maxOutputVertices - (inputBuffer.numPrevParticles * 6 + counter)); j++) {
+		int particleBufferSizeLeft = floor(inputBuffer.maxOutputVertices / 6) - (inputBuffer.numPrevParticles + counter);
+		int particlesToSpawn = inputBuffer.emitters[i].nrOfParticlesToSpawn;
+		if (inputBuffer.emitters[i].nrOfParticlesToSpawn > particleBufferSizeLeft) {
+			particlesToSpawn = particleBufferSizeLeft;
+			i = inputBuffer.numEmitters; // Break after this emitter because there is no more space for particles in the buffers
+		}
+		for (uint j = 0; j < particlesToSpawn; j++) {
 			float3 v0, v1, v2, v3;
 			
-			//Account for how far into the frame the spawn happened
-			float3 newVel = inputBuffer.emitters[i].velocity + inputBuffer.emitters[i].acceleration * inputBuffer.emitters[i].spawnTime;
+			v0 = inputBuffer.emitters[i].position + float3(-0.1, 0.1, 0.0);
+			v1 = inputBuffer.emitters[i].position + float3(-0.1, -0.1, 0.0);
+			v2 = inputBuffer.emitters[i].position + float3(0.1, 0.1, 0.0);
+			v3 = inputBuffer.emitters[i].position + float3(0.1, -0.1, 0.0);
 			
-			v0 = inputBuffer.emitters[i].position + float3(-0.1, 0.1, 0.0) + (inputBuffer.emitters[i].velocity + newVel) * 0.5 * inputBuffer.emitters[i].spawnTime;
-			v1 = inputBuffer.emitters[i].position + float3(-0.1, -0.1, 0.0) + (inputBuffer.emitters[i].velocity + newVel) * 0.5 * inputBuffer.emitters[i].spawnTime;
-			v2 = inputBuffer.emitters[i].position + float3(0.1, 0.1, 0.0) + (inputBuffer.emitters[i].velocity + newVel) * 0.5 * inputBuffer.emitters[i].spawnTime;
-			v3 = inputBuffer.emitters[i].position + float3(0.1, -0.1, 0.0) + (inputBuffer.emitters[i].velocity + newVel) * 0.5 * inputBuffer.emitters[i].spawnTime;
+			CSPhysicsBuffer[inputBuffer.numPrevParticles + counter].acceleration = inputBuffer.emitters[i].acceleration;
+			CSPhysicsBuffer[inputBuffer.numPrevParticles + counter].velocity = inputBuffer.emitters[i].velocity;
 			
-			CSPhysicsBuffer[inputBuffer.numPrevParticles + floor(counter / 6)].acceleration = inputBuffer.emitters[i].acceleration;
-			CSPhysicsBuffer[inputBuffer.numPrevParticles + floor(counter / 6)].velocity = newVel;
+			createParticle(v0, v1, v2, v3, inputBuffer.numPrevParticles + counter);
 			
-			createTriangle(v0, v1, v2, inputBuffer.numPrevParticles * 6 + counter);
-			counter += 3;
-			createTriangle(v2, v1, v3, inputBuffer.numPrevParticles * 6 + counter);
-			counter += 3;
+			//Physics for new particle (accounts for how far into the frame the particle was created)
+			updatePhysics(inputBuffer.numPrevParticles + counter, inputBuffer.emitters[i].spawnTime);
+			
+			counter++;
 		}
 	}
 	
-	for (uint i = 0; i < inputBuffer.numPrevParticles + counter / 6; i++) {
-		float3 oldVelocity = CSPhysicsBuffer[i].velocity;
-		CSPhysicsBuffer[i].velocity += CSPhysicsBuffer[i].acceleration * inputBuffer.frameTime;
-		for (uint j = 0 ; j < 6; j++) {
-			CSOutputBuffer[i * 6 + j].position += (oldVelocity + CSPhysicsBuffer[i].velocity) * 0.5 * inputBuffer.frameTime;
-		}
+	//Physics for all prevous particles
+	for (uint i = 0; i < inputBuffer.numPrevParticles; i++) {
+		updatePhysics(i, inputBuffer.frameTime);
 	}
 }
