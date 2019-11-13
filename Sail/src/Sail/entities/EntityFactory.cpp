d53ca130044d97aeba73bd0bfcadfa31263e91c9
Gustav Björk <gbjorken96@hotmail.com>
2019-11-13 12:38:01 +0100

    Enabled particles only for DEVELOPMENT

--- Sail/src/Sail/entities/EntityFactory.cpp
+++ Sail/src/Sail/entities/EntityFactory.cpp
@@ -38,6 +38,7 @@ void EntityFactory::CreateCandle(Entity::SPtr& candle, const glm::vec3& lightPos
 	candle->addComponent<BoundingBoxComponent>(boundingBoxModel);
 	candle->addComponent<CullingComponent>();
 
+#ifdef DEVELOPMENT
 	auto* particleEmitterComp = candle->addComponent<ParticleEmitterComponent>();
 	particleEmitterComp->position = { 4.0f, 1.0f, 0.0f };
 	particleEmitterComp->velocity = { 0.0f, 0.2f, 0.0f };
@@ -45,6 +46,7 @@ void EntityFactory::CreateCandle(Entity::SPtr& candle, const glm::vec3& lightPos
 	particleEmitterComp->spread = { 0.3f, 0.5f, 0.3f };
 	particleEmitterComp->spawnRate = 0.01f;
 	particleEmitterComp->lifeTime = 1.0f;
+#endif
 
 	PointLight pl;
 	pl.setColor(glm::vec3(1.0f, 0.7f, 0.4f));
@@ -98,6 +100,7 @@ Entity::SPtr EntityFactory::CreateMyPlayer(Netcode::PlayerID playerID, size_t li
 	myPlayer->addComponent<MovementComponent>()->constantAcceleration = glm::vec3(0.0f, -9.8f, 0.0f);
 	myPlayer->addComponent<RealTimeComponent>();
 
+#ifdef DEVELOPMENT
 	//For testing, add particle emitter to player.
 	auto* particleEmitterComp = myPlayer->addComponent<ParticleEmitterComponent>();
 	particleEmitterComp->position = { 0.0f, 2.0f, 0.0f };
@@ -106,6 +109,7 @@ Entity::SPtr EntityFactory::CreateMyPlayer(Netcode::PlayerID playerID, size_t li
 	particleEmitterComp->spread = {2.0f, 2.0f, 1.0f};
 	particleEmitterComp->spawnRate = 0.001f;
 	particleEmitterComp->lifeTime = 1.0f;
+#endif
 
 	AnimationComponent* ac = myPlayer->getComponent<AnimationComponent>();
 
