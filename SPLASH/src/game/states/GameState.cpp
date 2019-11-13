6d8c0e7c2b941b7e6f34d415103bcce0eb5dbf67
Emil <enukpenuk@hotmail.com>
2019-11-13 13:15:39 +0100

    Re-added multi-threading to runSystem.

--- SPLASH/src/game/states/GameState.cpp
+++ SPLASH/src/game/states/GameState.cpp
@@ -781,7 +781,7 @@ void GameState::updatePerFrameComponentSystems(float dt, float alpha) {
 }
 
 void GameState::runSystem(float dt, BaseComponentSystem* toRun) {
-	/*bool started = false;
+	bool started = false;
 	while (!started) {
 		// First check if the system can be run
 		if (!(m_currentlyReadingMask & toRun->getWriteBitMask()).any() &&
@@ -822,7 +822,7 @@ void GameState::runSystem(float dt, BaseComponentSystem* toRun) {
 				}
 			}
 		}
-	}*/
+	}
 	toRun->update(dt);
 }
 
