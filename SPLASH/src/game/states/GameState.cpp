d5dddfbc1c6cccee7e97062bac77488d5567b5d3
Alexander Wester <alexander.wester@gmail.com>
2019-11-23 00:40:38 +0100

    Fixes after merge

--- SPLASH/src/game/states/GameState.cpp
+++ SPLASH/src/game/states/GameState.cpp
@@ -885,9 +885,8 @@ void GameState::updatePerTickComponentSystems(float dt) {
 	m_componentSystems.gameInputSystem->fixedUpdate(dt);
 	m_componentSystems.spectateInputSystem->fixedUpdate(dt);
 
-	m_componentSystems.prepareUpdateSystem->update(); // HAS TO BE RUN BEFORE OTHER SYSTEMS WHICH USE TRANSFORM
 	m_componentSystems.prepareUpdateSystem->fixedUpdate(); // HAS TO BE RUN BEFORE OTHER SYSTEMS WHICH USE TRANSFORM
-	asdasd?
+	m_componentSystems.prepareUpdateSystem->update(); // HAS TO BE RUN BEFORE OTHER SYSTEMS WHICH USE TRANSFORM
 	m_componentSystems.lightSystem->prepareFixedUpdate();
 
 	
