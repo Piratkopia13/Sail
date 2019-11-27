7d7ce1698415c45515ee1d810c41a6d54093a9d7
Henrik Johansson <H3NX@hotmail.se>
2019-11-27 11:09:28 +0100

    fixed last commit

--- SPLASH/src/game/states/MenuState.cpp
+++ SPLASH/src/game/states/MenuState.cpp
@@ -627,7 +627,7 @@ void MenuState::renderProfile() {
 		static char buf[101] = "";
 		strncpy_s(buf, name.c_str(), name.size());
 		if (ImGui::InputText("##name", buf, MAX_NAME_LENGTH, ImGuiInputTextFlags_EnterReturnsTrue)) {
-			std::string name = buf;
+			name = buf;
 			while (name.find("%") != std::string::npos) {
 				name = name.erase(name.find("%"), 1);
 			}
