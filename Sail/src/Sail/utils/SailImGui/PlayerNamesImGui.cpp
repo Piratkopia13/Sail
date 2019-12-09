282baf2cc61549a7fcc4f0a6351416d3f7e513ca
Tobias F <fas_th@hotmail.com>
2019-12-09 10:55:43 +0100

    Fixed text rendering for spectators. and stuff

--- Sail/src/Sail/utils/SailImGui/PlayerNamesImGui.cpp
+++ Sail/src/Sail/utils/SailImGui/PlayerNamesImGui.cpp
@@ -23,6 +23,9 @@ void PlayerNamesImGui::renderWindow() {
 	float dist = 0;
 	float minDist = 3;
 	for (auto player : m_drawPlayers) {
+		if (!player || player->isAboutToBeDestroyed())
+			continue;
+
 		counter++;
 		TransformComponent* trans = player->getComponent<TransformComponent>();
 		BoundingBoxComponent* col = player->getComponent<BoundingBoxComponent>();
@@ -42,30 +45,54 @@ void PlayerNamesImGui::renderWindow() {
 			NetworkReceiverComponent* nrc = player->getComponent<NetworkReceiverComponent>();
 			if (nrc) {
 				Netcode::PlayerID owner = Netcode::getComponentOwner(nrc->m_id);
-
 				Player* p = NWrapperSingleton::getInstance().getPlayer(owner);
 				glm::vec3 color = Application::getInstance()->getSettings().getColor(Application::getInstance()->getSettings().teamColorIndex(p->team));
 				float alpha = 1;
 				float scale = 1.5;
 
-				if (m_localPlayer && m_maxDist > 0) {
+				float ww = Application::getInstance()->getWindow()->getWindowWidth();
+				float wh = Application::getInstance()->getWindow()->getWindowHeight();
+				bool outsideScreen = screenPos.x < 0 || screenPos.x > ww || screenPos.y < 0 || screenPos.y > wh;
+				bool wrongDirection = false;
+
+				screenPos.x = std::clamp(screenPos.x, 20.0f, ww - 10.0f);
+				screenPos.y = std::clamp(screenPos.y, 20.0f, wh - 10.0f);
+
+				if (m_localPlayer) {
 					TransformComponent* localTrans = m_localPlayer->getComponent<TransformComponent>();
 					if (localTrans) {
-						dist = glm::distance(localTrans->getRenderMatrix()[3], trans->getRenderMatrix()[3]);
-						if(dist < minDist){
-							alpha = 1;
+						glm::vec3 delta = trans->getRenderMatrix()[3] - localTrans->getRenderMatrix()[3];
+						wrongDirection = glm::dot(m_camera->getDirection(), delta) < -0.8f;
+						dist = glm::length(delta);
+						if (m_maxDist > 0) {
+							if(dist < minDist){
+								alpha = 1;
+							} else {
+								alpha = 1 - (dist - minDist) / m_maxDist;
+								scale = 1.5 - (dist - minDist) / m_maxDist;
+							}
 						} else {
-							alpha = 1 - (dist - minDist) / m_maxDist;
-							scale = 1.5 - (dist - minDist) / m_maxDist;
-						}
+							float f = std::max((dist - minDist) / 10.0f, 0.0f);
+							scale = std::max(1.5f - f, 1.0f);
+							screenPos.y -= std::min(20.0f * f, 10.0f);
+						}	
 					}
 				}
 
-				ImGui::PushStyleColor(0, ImVec4(color.x, color.y, color.z, alpha));
-				ImGui::SetWindowFontScale(scale);
-				ImGui::Text(p->name.c_str());
-				ImGui::SetWindowFontScale(1);
-				ImGui::PopStyleColor();
+				if (!wrongDirection) {
+					ImGui::PushStyleColor(0, ImVec4(color.x, color.y, color.z, alpha));
+					if (outsideScreen) {
+						ImGui::SetWindowFontScale(2);
+						ImGui::Text("*");
+						ImGui::SetWindowFontScale(1);
+					} else {
+						ImGui::SetWindowFontScale(scale);
+						ImGui::Text(p->name.c_str());
+						ImGui::SetWindowFontScale(1);
+					}
+					ImGui::PopStyleColor();
+				}
+
 
 			} else {
 				ImGui::Text("): !no name here! :(");
