0c2fc1fad81b66da3065d8d1ff2c91113cb8485d
Smaugmuel <samuelasp96@gmail.com>
2019-12-12 14:45:57 +0100

    Reduced spread of shower powerup

--- SPLASH/src/game/states/GameState.cpp
+++ SPLASH/src/game/states/GameState.cpp
@@ -227,6 +227,10 @@ GameState::GameState(StateStack& stack)
 		m_componentSystems.particleSystem->update(0);
 	}
 
+	PowerUpComponent::PowerUp pup;
+	pup.maxTime = 100;
+	pup.name = "shower";
+	m_player->getComponent<PowerUpComponent>()->powerUps[PowerUps::SHOWER] = pup;
 
 
 	// Keep this at the bottom
